0d02b8db78c0d3d28ced52538fbb8cec311231d9
Louis Qian <yuq4@illinois.edu>
2022-10-10 08:29:01 -0500

    Initial Commit

---
+++ Hackillinois iOS demo/Hackillinois_iOS_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackillinois_iOS_demoApp.swift
+//  Hackillinois iOS demo
+//
+//  Created by Louis Qian on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Hackillinois_iOS_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
