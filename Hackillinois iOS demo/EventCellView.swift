7b46fe6eecf989b08e3be403fc831625504ec88a
Louis Qian <yuq4@illinois.edu>
2022-10-10 21:05:14 -0500

    initial comit

---
+++ Hackillinois iOS demo/EventCellView.swift
@@ -0,0 +1,20 @@
+//
+//  EventCellView.swift
+//  Hackillinois iOS demo
+//
+//  Created by Louis Qian on 10/10/22.
+//
+
+import SwiftUI
+
+struct EventCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventCellView()
+    }
+}
---
+++ Hackillinois iOS demo/EventDetailViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  EventDetailViewModel.swift
+//  Hackillinois iOS demo
+//
+//  Created by Louis Qian on 10/10/22.
+//
+
+import SwiftUI
+
+struct EventDetailViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventDetailViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        EventDetailViewModel()
+    }
+}
